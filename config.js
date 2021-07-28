4762cfa3a5f261f4bdddf120a79bc0ea8dc6541d
BaoThen <88072458+BaoThen@users.noreply.github.com>
2021-07-28 13:12:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ const CONFIG = {
     introDesc: `Tui yêu Bồ nhất trên đời
     Tuy tui hay suy nghĩ này nọ nhưng
     Tui mong bồ hỉu đc lý do vì sao 
-    Tóm lại Em yêu Anhh nhiều nhiu`,
-    btnIntro: 'hihi'.
+    Tóm lại Em yêu Anhh nhiềuhiu`.
+    btnIntro: 'hihi',
     title: 'Vì đã lỡ yêu Bảo nhìu chắc dám hứa xa xôi 😙',
-    desc: 'Mong sao cho ta mãi bên nhau tới già nha'.
-    btnYes: 'Vẫn cứ là yêu bé nhìu <3'.
-    btnNo: 'hông, yêu bé nữa ;3'.
+    desc: 'Mong sao cho ta mãi bên nhau tới già nha',
+    btnYes: 'Vẫn cứ là yêu bé nhìu <3',
+    btnNo: 'hông, yêu bé nữa ;3',
     question: `Trên thế giới hơn 7 tỷ người mà sao anh lại yêu em <3'.
-    btnReply: 'Gửi cho em <3.'.
+    btnReply: 'Gửi cho em <3.',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
-    mess: 'Bé biết mà 🥰. Yêu Anh nhiều 😘😘.'.
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.'.
+    mess: 'Bé biết mà 🥰. Yêu Anh nhiều 😘😘.',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3'.`
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
