fa0ef32f9b42276e0405cd7428e49a8bd8bcd3b5
BaoThen <88072458+BaoThen@users.noreply.github.com>
2021-07-28 11:52:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const CONFIG = {
     Tuy tui hay suy nghĩ này nọ nhưng
     Tui mong bồ hỉu đc lý do vì sao 
     Tóm lại Em yêu Anhh nhiều nhiu`,
-    btnIntro: 'hihi',
+    btnIntro: 'hihi'.
     title: 'Vì đã lỡ yêu Bảo nhìu chắc dám hứa xa xôi 😙',
-    desc: 'Mong sao cho ta mãi bên nhau tới già nhaa ',
-    btnYes: 'Vẫn cứ là yêu bé nhìu <33',
-    btnNo: 'Không, yêu bé nữa :3',
+    desc: 'Mong sao cho ta mãi bên nhau tới già nhaa'.
+    btnYes: 'Vẫn cứ là yêu bé nhìu <33'.
+    btnNo: 'Không, yêu bé nữa ;3'.
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    btnReply: 'Gửi cho em <3'.
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Bé biết mà 🥰. Yêu Anh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.'.
